16cb544f423799fab17a9bdc99ca3b293adb6b30
xlzhu <szxlzhu@gmail.com>
2020-10-21 22:10:39 +0800

    1.4.2

--- part1/src/index.js
+++ part1/src/index.js
@@ -213,11 +213,16 @@ import React,{useState} from 'react'
 import ReactDom from 'react-dom'
 
 const Hello = (props) => {
+  const bornYear = () => {
+    const yearNow = new Date().getFullYear()
+    return yearNow - props.age
+  }
   return (
     <div>
       <p>
         Hello {props.name},you are {props.age} years old
       </p>
+  <p>So you were probably born in {bornYear()}</p>
     </div>
   )
 }
