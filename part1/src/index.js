fd56bc39b579044fcbaef913ca098e4c6c7b4892
xlzhu <szxlzhu@gmail.com>
2020-11-04 23:52:11 +0800

    1.0.9

--- part1/src/index.js
+++ part1/src/index.js
@@ -26,12 +26,12 @@ const App = () => {
   const age = 10
 
   return (
-    <div>
+    <>
       <h1>Greetings</h1>
       <Hello name="Maya" age={26 + 10} />
       <Hello name={name} age={age} />
       <Footer/>
-    </div>
+    </>
   )
 }
 
